ef8fa802616c1aa88d8969c86733a789cd635299
HpSharma <harpalhmh@gmail.com>
2025-04-29 22:48:35 -0400

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,12 @@
-import Text from './Text';
+import Input from "./Input.jsx";
 
 function App() {
 
-  console.log('Hello from APP');
-
   return (
-   <div>
-     <Text name={"Sanjay"} age={40} />
-     <h1>Hello</h1>
+   <div style={{padding: '50px 100px'}}>
+     <Input></Input>
    </div>
   )
 }
 
 export default App;
-
-
-// export default function App() {
-//   return (
-//     <h1>Hello World!</h1>
-//   )
-// }
-
-
-// const App = () => {
-//   return (
-//     <h1>Hello World! From</h1>
-//   )
-// }
-//
-// export default App;
-
-
-// import {Component} from "react";
-//
-// class App extends Component {
-//   constructor() {
-//     super();
-//   }
-//
-//   render() {
-//     return (
-//       <h1>Hello from class</h1>
-//     )
-//   }
-// }
-//
-// export default App;
---
+++ src/Input.jsx
@@ -0,0 +1,69 @@
+/*
+// ------------------- Class based Component -------------------
+
+import {Component} from "react";
+
+class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Hello State",
+    }
+    this.getValue.bind(this);
+  }
+
+  getValue() {
+    return this.state.value;
+  }
+
+  render() {
+    console.log(this.state);
+    this.state.value = "HELLO";
+    return (
+      <>
+        <h1>Hi Input {this.state.value} In {this.props.name}</h1>
+        <h3>{this.getValue()}</h3>
+      </>
+    )
+  }
+
+}
+*/
+
+
+import {useState} from "react";
+
+function Input() {
+  let [value, setValue] = useState("");
+
+  console.log({value});
+
+  // const handleValueChange = (event) => {
+  //   console.log(event.target.value);
+  //   setValue(event.target.value);
+  // }
+
+  // const handleValueChange = (value) => {
+  //   setValue(value);
+  // }
+
+  return (
+    <>
+      <input
+        type={"text"}
+        placeholder={"Enter your name..."}
+        value={value}
+        // onChange={(event) => handleValueChange(event)}
+        // onChange={handleValueChange}
+        // onChange={(event) => handleValueChange(event.target.value)}
+        onChange={(event) => setValue(event.target.value)}
+      />
+
+      <h4>You have entered</h4>
+      <span>{value}</span>
+    </>
+  )
+}
+
+
+export default Input;
