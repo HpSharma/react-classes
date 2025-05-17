b49bdcda744850d104947e29ebe83e796f6baf07
HpSharma <harpalhmh@gmail.com>
2025-05-17 14:51:57 -0400

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import {useState} from "react";
-import {faUser, faSpinner} from "@fortawesome/free-solid-svg-icons";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
 
 import TextInput from "./Component/TextInput";
 import {Button} from "./Component/Button/index.jsx";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import SelectInput from "./Component/SelectInput/index.jsx";
 import Modal from "./Component/Modal/index.jsx";
 
 function App() {
   const [name, setName] = useState("");
-  const [nameError, setNameError] = useState(false);
   const [openModal, setOpenModal] = useState(false);
-
-  const handleNameChange = (value) => {
-
-  }
 
   return (
     <div style={{padding: 50, display: 'flex', flexDirection: 'column', gap: '25px'}}>
@@ -22,11 +16,10 @@ function App() {
         required
         label={"Name"}
         value={name}
-        hasError={nameError}
         helperText={"Please enter your full name"}
         placeholder={"Enter your name"}
         icon={faUser}
-        onChange={handleNameChange}/>
+        onChange={(v) => setName(v)}/>
 
       <Button variant={"destructive"} onClick={() => setOpenModal(true)}>
         Open Modal
@@ -39,11 +32,10 @@ function App() {
           required
           label={"Name"}
           value={name}
-          hasError={nameError}
           helperText={"Please enter your full name"}
           placeholder={"Enter your name"}
           icon={faUser}
-          onChange={handleNameChange}/>
+          onChange={(v) => setName(v)}/>
 
       </Modal>
 
